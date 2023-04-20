5d93ead35391920fd66483592782ad8924953eab
Denis412 <d-a-malyshev@yandex,ru>
2023-04-20 18:51:01 +0300

    Создан компонент ClubInput, добавлены функции валидации для него, реализованы модификаторы capitalize и capitalizeAll для v-model

---
+++ src/components/ClubInput.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-input
+    class="club-input"
+    outlined
+    v-model="value"
+    @update:modelValue="emitChange"
+    :type="calcType"
+    :placeholder="placeholder"
+    :rules="rules"
+  >
+    <template v-slot:append v-if="visibility">
+      <q-img
+        class="cursor-pointer club-input-append-icon"
+        src="src/assets/icons/Eye.svg"
+        @click="toggleShowText"
+      />
+    </template>
+  </q-input>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import capitalizeWord from "src/utils/capitalizeWord";
+
+const { type, placeholder, visibility, rules, modelValue, modelModifiers } =
+  defineProps({
+    type: String,
+    placeholder: String,
+    visibility: Boolean,
+    rules: Array,
+    modelValue: String,
+    modelModifiers: {
+      default: () => ({}),
+    },
+  });
+const emit = defineEmits(["update:modelValue"]);
+
+const showText = ref(visibility);
+const value = ref("");
+const calcType = ref(type);
+
+const toggleShowText = () => {
+  showText.value = !showText.value;
+
+  calcType.value = showText.value ? "password" : "text";
+};
+
+const emitChange = (targetValue) => {
+  let text = targetValue;
+
+  if (modelModifiers.capitalize) text = capitalizeWord(text);
+
+  if (modelModifiers.capitalizeAll) {
+    let words = text.split(" ");
+
+    text =
+      words.length > 1
+        ? words.map((word) => capitalizeWord(word.trim())).join(" ")
+        : capitalizeWord(words[0]);
+  }
+
+  value.value = text;
+  emit("update:modelValue", text);
+};
+</script>
+
+<style scoped lang="scss">
+.club-input-append-icon {
+  width: 22px;
+  height: 15px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,30 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center column">
+    <!-- <t-club-input type="password" placeholder="Введите пароль" /> -->
+    <q-form @submit="sub">
+      <club-input
+        type="password"
+        visibility
+        placeholder="Введите пароль"
+        :rules="[required, minLength(8), passwordValid]"
+        v-model="v"
+      />
+
+      <q-btn label="Submit" type="submit" />
+    </q-form>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref } from "vue";
+import ClubInput from "src/components/ClubInput.vue";
+import { useValidators } from "src/use/validators";
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const { passwordValid, minLength, required } = useValidators();
+
+const sub = () => {
+  console.log("gshg");
+};
+
+const v = ref("f");
 </script>
