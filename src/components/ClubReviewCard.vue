9b7d0b567bbeb0e929229a20d78d3a826bdc1494
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2023-04-21 21:22:13 +0300

    Доработка блока комментариев

--- src/components/ClubReviewCard.vue
+++ src/components/ClubReviewCard.vue
@@ -1,27 +1,27 @@
 <template>
   <q-card class="card">
     <div class="flex items-center">
-      <q-img style="width: 90px" :src="avatarUrl" alt="avatar" />
+      <q-img class="avatar" :src="avatarUrl" alt="avatar" />
 
-      <div style="margin-left: 50px">
+      <div class="title-block" >
         <div class="font-size-24">{{ fullname }}</div>
 
-        <div class="card-job-title" style="margin-top: 8px">
+        <div class="card-job-title">
           {{ jobTitle }}
         </div>
       </div>
     </div>
 
-    <div style="margin-top: 32px; overflow-y: auto">
+    <div class="comment-content">
       {{ content }}
     </div>
 
-    <div style="margin-top: 24px">
+    <div class="stars-area">
       <q-icon
         v-for="n in 5"
+        class="star"
         name="star"
         :key="n"
-        style="font-size: 22px"
         :style="{ color: n <= stars ? '#B936E7' : '#190A32' }"
       />
     </div>
@@ -47,8 +47,12 @@ const { avatarUrl, fullname, jobTitle, content, stars } = defineProps({
   height: 350px;
   background: #260f4a;
   padding: 40px;
-
+  margin: 0 0 32px 30px;
   border-radius: 33px;
+
+  &:first-child {
+    margin: 0 0 30px 0;
+  }
 
   &-job-title {
     background: linear-gradient(101.75deg, #881d95 4.25%, #881d95 96.95%);
@@ -56,6 +60,28 @@ const { avatarUrl, fullname, jobTitle, content, stars } = defineProps({
     -webkit-text-fill-color: transparent;
     background-clip: text;
     text-fill-color: transparent;
+    margin-top: 8px;
   }
+}
+
+.avatar {
+  width: 90px
+}
+
+.title-block {
+  margin-left: 50px
+}
+
+.comment-content {
+  margin-top: 32px;
+  overflow-y: auto;
+}
+
+.star {
+  font-size: 22px;
+}
+
+.stars-area {
+  margin-top: 24px
 }
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -15,19 +15,8 @@
       <partners/>
       <join/>
       <products/>
+      <comments/>
 
-      <section class="flex column flex-center section-margin">
-        <div class="header-text-32 text-center">Отзывы</div>
-
-        <q-list class="flex flex-center no-wrap" style="
-                                  margin-left: -30px;
-                                  margin-top: 32px;
-                                  overflow-x: auto;
-                                  max-width: 1200px;
-                                ">
-          <review-card style="margin-left: 30px" v-for="comment in comments" :key="comment.id" v-bind="comment" />
-        </q-list>
-      </section>
     </main>
   </q-page>
 </template>
@@ -44,61 +33,9 @@ import Clients from "src/components/ClubClients.vue";
 import Partners from "src/components/ClubPartners.vue";
 import Join from "src/components/ClubJoin.vue";
 import Products from "src/components/ClubProducts.vue";
+import Comments from "src/components/ClubComments.vue"
 import BackgroundSpot from "src/components/BackgroundSpot.vue";
-import ReviewCard from "src/components/ReviewCard.vue";
-import { ref } from "vue";
 
-
-
-
-
-const comments = ref([
-  {
-    id: 1,
-    avatarUrl: "src/assets/images/Ellipse55.svg",
-    fullname: "Дмитрий Соколов",
-    jobTitle: "Дизайнер",
-    content:
-      "Честно говоря, я был сильно удивлен, что несмотря на разные результаты, масштабы бизнеса, опыт, все с кем мне приходилось общаться в клубе, невероятно доброжелательные, открытые и близкие по духу люди. также с помощью клуба нашел двух специалистов для решения своей задачи.",
-    stars: 4,
-  },
-  {
-    id: 2,
-    avatarUrl: "src/assets/images/Ellipse54.svg",
-    fullname: "Александр Иванов",
-    jobTitle: "Менеджер",
-    content:
-      "Я стал частью большой команды. Для меня это личностный рост и постоянная поддержка. Могу обратиться с любым вопросом и меня услышат. Начал находить решения, как мне эффективнее продвигаться по моим задачам. Сейчас активно принимаю участие в проекте. Я убедился, что замыкаться в своей области и в своем кругу знакомств не дают развития. За это моя благодарность 1Т Клубу.",
-    stars: 5,
-  },
-  // {
-  //   id: 3,
-  //   avatarUrl: "src/assests/images/Ellipse55.svg",
-  //   fullname: "Александр Иванов",
-  //   jobTitle: "Менеджер",
-  //   content:
-  //     "Честно говоря, я был сильно удивлен, что несмотря на разные результаты, масштабы бизнеса, опыт, все с кем мне приходилось общаться в клубе, невероятно доброжелательные, открытые и близкие по духу люди. также с помощью клуба нашел двух специалистов для решения своей задачи.",
-  //   stars: 5,
-  // },
-  // {
-  //   id: 4,
-  //   avatarUrl: "src/assests/images/Ellipse55.svg",
-  //   fullname: "Александр Иванов",
-  //   jobTitle: "Менеджер",
-  //   content:
-  //     "Честно говоря, я был сильно удивлен, что несмотря на разные результаты, масштабы бизнеса, опыт, все с кем мне приходилось общаться в клубе, невероятно доброжелательные, открытые и близкие по духу люди. также с помощью клуба нашел двух специалистов для решения своей задачи.",
-  //   stars: 5,
-  // },
-  // {
-  //   id: 5,
-  //   avatarUrl: "src/assests/images/Ellipse55.svg",
-  //   fullname: "Александр Иванов",
-  //   jobTitle: "Менеджер",
-  //   content:
-  //     "Честно говоря, я был сильно удивлен, что несмотря на разные результаты, масштабы бизнеса, опыт, все с кем мне приходилось общаться в клубе, невероятно доброжелательные, открытые и близкие по духу люди. также с помощью клуба нашел двух специалистов для решения своей задачи.",
-  //   stars: 5,
-  // },
-]);
 </script>
 
 <style scoped lang="scss">
