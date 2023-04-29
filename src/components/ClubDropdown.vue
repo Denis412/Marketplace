4d2811a7dd957bcd17c748d699f43ea5c9bf66c3
Denis412 <d-a-malyshev@yandex,ru>
2023-04-29 23:24:22 +0300

    Добавлен компонент ClubDropdown

---
+++ src/components/ClubDropdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-btn-dropdown
+    v-model="menu"
+    flat
+    no-caps
+    class="q-ml-lg club-dropdown text-body2"
+    label="Dropdown"
+    dropdown-icon="img:/src/assets/icons/arrow/arrow-down-grey.svg"
+  >
+    <slot name="list"></slot>
+  </q-btn-dropdown>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const { list } = defineProps({
+  list: Array,
+});
+
+const menu = ref(false);
+
+const onItemClick = () => {};
+</script>
+
+<style scoped lang="scss"></style>
--- src/pages/ClubTeamsPage.vue
+++ src/pages/ClubTeamsPage.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="c-pa-32 page-background flex flex-center" style="z-index: 0">
-    <c-not-found-teams />
+    <!-- <c-not-found-teams /> -->
 
     <!-- <section class="flex flex-center"> -->
-    <c-create-team-form />
+    <!-- <c-create-team-form /> -->
     <!-- </section> -->
     <!-- <c-team-profile :team="team" /> -->
   </q-page>
