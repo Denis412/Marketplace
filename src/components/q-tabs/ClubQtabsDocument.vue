54ee91561712260fd8feb3a58fd8f062ca5fe76e
Sheradab Urakov <sheradaburakov@gmail.com>
2023-05-01 16:22:51 +0300

    Появление btn-dropdown при наведении

--- src/components/q-tabs/ClubQtabsDocument.vue
+++ src/components/q-tabs/ClubQtabsDocument.vue
@@ -1,41 +1,22 @@
 <template>
-  <q-tabs
-    class="row no-wrap q-pl-lg drawer-item"
-    indicator-color="transparent"
-    v-for="(doc, index) in FILES"
-    :key="doc.id"
-    align="left"
-  >
+  <q-tabs class="row no-wrap q-pl-lg drawer-item" indicator-color="transparent" v-for="(doc, index) in FILES"
+    :key="doc.id" align="left">
     <q-route-tab>
       <div class="item_doc">
-        <img
-          :src="`/src/assets/icons/file/file-grey.svg`"
-          alt=""
-          class="q-pr-md"
-        />
-        <router-link
-          class="item_doc link"
-          :to="{
+        <img :src="`/src/assets/icons/file/file-grey.svg`" alt="" class="q-pr-md" />
+        <router-link class="item_doc link" :to="{
             name: 'Document',
             params: { id: `${index}` },
-          }"
-        >
+          }">
           {{
             doc.name.replace(".html", "").length > 10
-              ? doc.name.replace(".html", "").slice(0, 10) + "..."
-              : doc.name.replace(".html", "")
+            ? doc.name.replace(".html", "").slice(0, 10) + "..."
+            : doc.name.replace(".html", "")
           }}
         </router-link>
         <div class="menu-wrapper" clickable>
-          <q-btn-dropdown
-            no-icon-animation
-            dropdown-icon="more_vert"
-            size="sm"
-            no-caps
-            unelevated
-            no-wrap
-            label=""
-          >
+          <q-btn-dropdown no-icon-animation dropdown-icon="more_vert" size="sm" no-caps unelevated no-wrap label=""
+            class="btn-dropdown-doc">
             <c-qmenu-document :prop_clicked_index_doc="index" :prop_doc="doc" />
           </q-btn-dropdown>
         </div>
@@ -63,16 +44,27 @@ watch(FILES, () => {
 .menu-wrapper {
   width: 40px;
 }
+
 .item_doc {
   width: 190px;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+
 .link {
   text-decoration: none;
 }
+
 .link:visited {
   color: inherit;
+}
+
+.btn-dropdown-doc {
+  display: none;
+}
+
+.item_doc:hover .btn-dropdown-doc {
+  display: flex;
 }
 </style>
