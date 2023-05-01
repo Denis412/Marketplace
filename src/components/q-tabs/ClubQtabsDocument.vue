645622a4e3686b38e49378147be15e5961f7fbea
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-05-01 13:17:29 +0300

    Вынес поп-ап в отдельный компонент

--- src/components/q-tabs/ClubQtabsDocument.vue
+++ src/components/q-tabs/ClubQtabsDocument.vue
@@ -2,120 +2,20 @@
   <q-tabs
     class="row no-wrap q-pl-lg drawer-item"
     indicator-color="transparent"
-    v-for="(doc, index) in FILES"
+    v-for="(doc) in FILES"
     :key="doc.id"
     align="left"
   >
-    <q-route-tab
-      :to="{
-        name: 'Document',
-        params: { id: `${index}` },
-      }"
-      
-    >
+    <q-route-tab>
       <div class="item_doc">
         <img :src="`/src/assets/icons/file/file-grey.svg`" alt="" class="q-pr-md"/>
-        <div class="item_doc">{{ (doc.name.replace('.html', '').length > 10) ? doc.name.replace('.html', '').slice(0, 10) + '...' : doc.name.replace('.html', '') }} </div>
-        
+        <div class="item_doc">
+          {{ (doc.name.replace('.html', '').length > 10) ? doc.name.replace('.html', '').slice(0, 10) + '...' : doc.name.replace('.html', '') }}
+        </div>
         <div class="menu-wrapper" clickable>
-          ⋮
-          <q-menu class="popup" anchor="bottom right" self="top left">
-            <q-item class="popup-component" clickable>
-              <q-img
-                class="popup-png"
-                src="/src/assets/icons/doc_popup/link.png"
-              />
-              <q-item-section>Открыть</q-item-section>
-            </q-item>
-
-            <q-item class="popup-component" clickable>
-              <q-img
-                class="popup-png"
-                src="/src/assets/icons/doc_popup/file.png"
-              />
-              <q-item-section>Дублировать</q-item-section>
-            </q-item>
-
-            <q-item class="popup-component" clickable>
-              <q-img
-                class="popup-png2"
-                src="/src/assets/icons/doc_popup/trash.png"
-              />
-              <q-item-section @click="showDialog = true"
-                >Удалить</q-item-section
-              >
-            </q-item>
-
-            <q-dialog v-model="showDialog">
-                <q-card>
-                  <q-card-section>
-                    <div class="text-h6">Удаление файла</div>
-                  </q-card-section>
-
-                  <q-card-section class="q-pt-none">
-                    Вы уверены, что хотите удалить этот файл? Отменить это действие будет невозможно
-                  </q-card-section>
-
-                  <q-card-actions align="right">
-                    <q-btn label="Да" @click="filesApi.deleteDoc(doc.id), showDialog = false" />
-                    <q-btn label="Нет" @click="showDialog = false" />
-                  </q-card-actions>
-                  </q-card>
-              </q-dialog>
-
-            <q-item class="popup-component" clickable>
-              <q-img
-                class="popup-png"
-                src="/src/assets/icons/doc_popup/lock.png"
-              />
-              <q-item-section>Права доступа</q-item-section>
-            </q-item>
-
-            <q-item class="popup-component" clickable>
-              <q-img
-                class="popup-png"
-                src="/src/assets/icons/doc_popup/pen.png"
-              />
-              <q-item-section>Переименовать</q-item-section>
-              <q-popup-edit
-                v-model="doc.name"
-                :validate="(val) => val.length < 150"
-                v-slot="scope"
-              >
-                <q-input
-                  v-model="scope.value"
-                  :model-value="scope.value"
-                  hint="Введите имя документа"
-                  :rules="[
-                    (val) => scope.validate(val) || 'Слишком длинное название',
-                  ]"
-                >
-                  <template v-slot:after>
-                    <q-btn
-                      flat
-                      dense
-                      color="negative"
-                      icon="cancel"
-                      @click.stop.prevent="scope.cancel"
-                    />
-                    <q-btn
-                      flat
-                      dense
-                      color="positive"
-                      icon="check_circle"
-                      @click.stop.prevent="
-                        filesApi.updateFile(scope.value, doc)
-                      "
-                      :disable="
-                        scope.validate(scope.value) === false ||
-                        scope.initialValue === scope.value
-                      "
-                    />
-                  </template>
-                </q-input>
-              </q-popup-edit>
-            </q-item>
-          </q-menu>
+          <q-btn-dropdown no-icon-animation dropdown-icon="more_vert" size="sm" no-caps unelevated no-wrap label="">
+            <c-qmenu-document :prop_doc = "doc"/>
+          </q-btn-dropdown>
         </div>
       </div>
     </q-route-tab>
@@ -123,16 +23,11 @@
 </template>
 
 <script setup>
-import { defineComponent, ref, computed, onMounted, watch } from "vue";
-import { useQuery } from "@vue/apollo-composable";
-import { provideApolloClient } from "@vue/apollo-composable";
-import { useMutation } from "@vue/apollo-composable";
-import { useQuasar } from "quasar";
+import { computed, watch } from "vue";
 import { useFileStore } from "src/stores/file";
-import filesApi from "src/sdk/file";
+import CQmenuDocument from "./ClubQmenuDocument.vue";
 
 const storeFile = useFileStore();
-let showDialog = ref(false);
 const FILES = computed(() => storeFile.GET_FILES);
 
 watch(FILES, () => {
@@ -141,9 +36,6 @@ watch(FILES, () => {
 </script>
 
 <style scoped lang="scss">
-.q-tab__content {
-  width: 100%;
-}
 .menu-wrapper {
   width: 40px;
 }
@@ -154,50 +46,4 @@ watch(FILES, () => {
   align-items: center;
 }
 
-.q-tabs__content {
-  width: 100% !important;
-  text-align: left !important;
-}
-.link {
-  color: white;
-  text-decoration: none;
-}
-.popup {
-  align-items: center;
-  padding: 8px 0px 20px;
-  gap: 16px;
-  position: relative;
-  width: 290px;
-  height: 291px;
-  border: 1px solid #bbbbbb;
-  box-shadow: 0px 0px 45px rgba(0, 0, 0, 0.06);
-  border-radius: 12px;
-}
-.popup-component {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 10px 29px 10px 20px;
-  gap: 18px;
-  width: 290px;
-  height: 40px;
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 20px;
-  color: #666666;
-}
-.popup-png {
-  width: 16px;
-  height: 14px;
-}
-.popup-png2 {
-  width: 14px;
-  height: 16px;
-}
-.doc_wrapper {
-  display: flex;
-  justify-content: space-between;
-}
 </style>
