5f6ce7dfe0641e346566ff8ee3e1637a0a770f0c
Andrew Nasibullin <duhich02@yandex.ru>
2023-05-03 00:13:39 +0300

    Добавил карточки заказов криво расположенные. Нужно еще добавить цвет статусам и кнопку редактирования

--- src/components/ClubOrderItem.vue
+++ src/components/ClubOrderItem.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
+  <div class="item">
     <q-page class="q-pa-md">
-      <q-card class="item">
+      <q-card class="oItem">
         <q-card-section class="oNum">
-          1260
+          {{ item.number }}
         </q-card-section>
         <q-card-section class="oName">
-          Лендинг чайного магазина
+          {{ item.name }}
         </q-card-section>
         <q-card-section class="oState">
-          Поиск исполнителя
+          {{ item.state }}
         </q-card-section>
         <q-card-section class="oDateUpdate">
-          24.05.2023
+          {{ item.date }}
         </q-card-section>
         <q-card-section class="oResponse">
           <img src="../assets/icons/orderIcons/deal.png" alt="img">
@@ -21,7 +21,7 @@
           <img src="../assets/icons/orderIcons/wait.png" alt="img">
         </q-card-section>
         <q-card-section class="oEdit">
-          <img src="../assets/icons/orderIcons/tripleDot.png" alt="img">
+          <button><img src="../assets/icons/orderIcons/tripleDot.png" alt="img"></button>
         </q-card-section>
       </q-card>
     </q-page>
@@ -30,31 +30,35 @@
 
 <script setup>
 
+const { item } = defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+});
+
 </script>
 
 <style scoped>
 .item{
+  max-height: 140px;
+}
+.oItem{
   background-color: #DFD1F6;
   width: 1109px;
   display: flex;
   justify-content:space-between;
   flex-direction: row;
-  vertical-align: middle;
-  text-align: center;
-  line-height: normal;
 }
-.q-card__section--vert {
-  padding: 23px;
-}
-.oNum, .oName, .oState, .oDateUpdate{
-  padding: 30px;
+.oNum, .oName, .oState, .oDateUpdate, .oResponse, .oDeal, .oEdit{
+  padding: 20px;
   font-family: 'Montserrat';
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   line-height: 17px;
 }
-.oResponse, .oDeal, .oEdit{
-  line-height: 16px;
+.oName{
+  max-width: 165px;
 }
 </style>
--- src/components/ClubOrders.vue
+++ src/components/ClubOrders.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    <c-orders-item/>
+    <c-orders-item
+      v-for="item in items"
+      :item="item"
+      :key="item.id"
+    />
   </div>
 </template>
 
 <script setup>
 import COrdersItem from 'src/components/ClubOrderItem.vue'
+import {ref} from "vue";
+
+const items = ref([
+  { number: "1260", name: "Лендинг чайного магазина", state: "Поиск исполнителя", date: "24.05.2023" },
+  { number: "1257", name: "Создание платформы для начинающих мастеров", state: "Есть кандидат", date: "20.05.2023" },
+  { number: "1255", name: "Поддержка программного обеспечения фитнесс центра ", state: "Выполняется", date: "19.05.2023" },
+  { number: "1250", name: "Обеспечение сохранности и безопасности данных агенства недвижимости", state: "Завершен", date: "18.05.2023" },
+  { number: "1249", name: "Оптимизация ИТ-инфраструктуры для банка «Яблоко банк»", state: "Отменен заказчиком", date: "17.05.2023" },
+  { number: "1247", name: "Миграция на новые годные ИТ-решения нашей строительной компании «Кирпич»", state: "Черновик", date: "16.05.2023" },
+]);
 
 </script>
-
 <style scoped>
-
 </style>
