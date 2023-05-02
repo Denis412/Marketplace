8eb3fb77bce1f20d1abc08d2a174ee919dcfb626
SofiyaKH <119045257+SofiyaKH@users.noreply.github.com>
2023-05-02 20:16:24 +0300

    Динамический массив для вывода всех команд на странице SearchTeamPage

--- src/graphql/team/queries.js
+++ src/graphql/team/queries.js
@@ -1,11 +1,32 @@
 import gql from "graphql-tag"
 
-//отображение команд как пространства
-export const getTeams = gql`query User($id: String!) {
-    user(id: $id) {
-        spaces
+
+export const getTeams = gql`
+query getTeams {
+    paginate_Teams(page: 1, perPage: 100) {
+      data {
+        id
+        type_id
+        author_id
+        level
+        position
+        created_at
+        updated_at
+        name
+        description
+      }
+      paginatorInfo {
+        perPage
+        currentPage
+        lastPage
+        total
+        count
+        from
+        to
+        hasMorePages
+      }
     }
-},`
+  }`
 
 export const filterTeamsName = gql`
 query filterTeamsName($where: ObjectPaginatorWhere) {
--- src/sdk/team.js
+++ src/sdk/team.js
@@ -1,44 +1,52 @@
 import { provideApolloClient, useMutation, useQuery } from "@vue/apollo-composable";
+import { data } from "autoprefixer";
 import apolloClient from "src/apollo/apollo-client";
 import { teamCreate, teamUpdate } from "src/graphql/team/mutations";
-import { filterTeamsName } from "src/graphql/team/queries";
+import { filterTeamsName, getTeams } from "src/graphql/team/queries";
 
 provideApolloClient(apolloClient);
 
 const { mutate: createTeam } = useMutation(teamCreate);
 const { mutate: updateTeam } = useMutation(teamUpdate);
+
+const { refetch: refetchAllTeams } = useQuery(getTeams);
 const { refetch: refetchTeams } = useQuery(filterTeamsName, {
-    where: {
-      column: "name",
-      operator: "EQ",
-      value: ""
-    }
+  where: {
+    column: "name",
+    operator: "EQ",
+    value: ""
   }
+}
 );
 
+const getAllTeams = async () => {
+  const { data: allTeams } = await refetchAllTeams();
+  return allTeams.paginate_Teams.data
+};
+
 const userTeamCreate = async ({ name, description }) => {
-    const {data: userTeamInfo } = await createTeam({
-        input: {
-            name,
-            description,
-          },
-    });
+  const { data: userTeamInfo } = await createTeam({
+    input: {
+      name,
+      description,
+    },
+  });
 
-    return userTeamInfo.create_Teams.record.id
-  };
+  return userTeamInfo.create_Teams.record.id
+};
 
-  const userTeamUpdate = async (id, team_img, name) => {
-    console.log(id, typeof(team_img))
-    const {data: userUpdateTeamInfo } = await updateTeam({
-      id,
-        input: {
-          name,
-            team_img,
-          },
-    });
-    console.log(userUpdateTeamInfo.update_Teams.status)
-    return userUpdateTeamInfo.update_Teams.status
-  };
+const userTeamUpdate = async (id, team_img, name) => {
+  console.log(id, typeof (team_img))
+  const { data: userUpdateTeamInfo } = await updateTeam({
+    id,
+    input: {
+      name,
+      team_img,
+    },
+  });
+  console.log(userUpdateTeamInfo.update_Teams.status)
+  return userUpdateTeamInfo.update_Teams.status
+};
 
 const checkName = async ({ name }) => {
   const { data: filteredTeam } = await refetchTeams(
@@ -52,8 +60,8 @@ const checkName = async ({ name }) => {
   return (filteredTeam.paginate_Teams.paginatorInfo.count == 0)
 }
 
-  const userTeams = {
-    userTeamCreate, userTeamUpdate, checkName
-  };
-  
-  export default userTeams;
+const userTeams = {
+  userTeamCreate, userTeamUpdate, checkName, getAllTeams
+};
+
+export default userTeams;
