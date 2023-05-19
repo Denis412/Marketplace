dead3872e5c12247c1e525c79ab9f06c715a159e
Shidorix <danila.mazepa@gmail.com>
2023-05-19 20:26:10 +0300

    Полностью поменял компонент ClubTaskeTable. Изменена структура, стилистика и многое другое.

--- src/components/ClubTaskTable.vue
+++ src/components/ClubTaskTable.vue
@@ -1,52 +1,49 @@
 <template>
-  <h3 class="text-h3 q-ma-xl">Мои задачи</h3>
-  <q-table
-    class="q-mx-lg no-shadow flat"
-    :rows="tasks"
-    :columns="data.columns"
-    :rows-per-page-options="[5, 10, 20]"
-    dense
-  >
-    <template v-slot:body="props">
-      <q-tr :props="props">
-        <q-td
-          v-if="props.row.first"
-          @click="addNewTask"
-          class="items-centered addNewTask"
-        >
-          <q-icon name="add" align="left" />
-          <span class="q-pl-sm">Новая задача</span>
-        </q-td>
-        <q-td class="text-subtitle3" v-else>{{ props.row.name }}</q-td>
-        <q-td class="text-subtitle3" v-if="!props.row.first">-</q-td>
-        <q-td v-else></q-td>
-        <q-td class="text-subtitle3">{{ props.row.dateStart?.date }}</q-td>
-        <q-td class="text-subtitle3">{{ props.row.dateEnd?.date }}</q-td>
-        <q-td>
-          <div
-            :class="
-              props.row.taskStatus == '8407796538990824904'
-                ? 'assigned'
-                : props.row.taskStatus == '7045273205012284690'
-                ? 'accomplished'
-                : 'completed'
-            "
-          >
-            {{
-              (function () {
-                if (props.row.taskStatus == "8407796538990824904") {
-                  return "надо сделать";
-                } else if (props.row.taskStatus == "7045273205012284690")
-                  return "в процессе";
-                else if (props.row.taskStatus == "2406017079472962662")
-                  return "выполнено";
-              })()
-            }}
+  <div>
+    <h3 class="text-h3 q-ma-xl col">Мои задачи</h3>
+    <q-card class="no-shadow">
+      <q-card-section class="row flat">
+        <div class="col-12 border-bottom">
+          <div class="row q-pb-md">
+            <div class="col-4 text-subtitle3">Задача</div>
+            <div class="col-2 text-subtitle3">Проект</div>
+            <div class="col-2 text-subtitle3">Дата начала</div>
+            <div class="col-2 text-subtitle3">Дата окончания</div>
+            <div class="col-2 text-subtitle3">Статус</div>
           </div>
-        </q-td>
-      </q-tr>
-    </template>
-  </q-table>
+        </div>
+      </q-card-section>
+    </q-card>
+    <q-card
+      v-for="row in tasks"
+      :key="row.name"
+      class="no-shadow q-ma-md bg-color q-pa-lg"
+      :class="{ 'my-color': getStatusText(row.taskStatus) === 'надо сделать' }"
+    >
+      <q-card-section class="row">
+        <div class="col-4 text-subtitle3">{{ row.name }}</div>
+        <div class="col-2 text-subtitle3">-</div>
+        <div class="col-2 text-subtitle3">{{ row.dateStart?.date }}</div>
+        <div class="col-2 text-subtitle3">{{ row.dateEnd?.date }}</div>
+        <div class="col-2 text-subtitle3">
+          <template v-if="getStatusText(row.taskStatus) === 'надо сделать'">
+            <q-icon name="error" class="text-red" />
+            <span class="my-text-blue">{{
+              getStatusText(row.taskStatus)
+            }}</span>
+          </template>
+          <template v-else-if="getStatusText(row.taskStatus) === 'в процессе'">
+            <q-icon name="autorenew" class="text-orange" />
+            <span class="text-orange">{{ getStatusText(row.taskStatus) }}</span>
+          </template>
+          <template v-else-if="getStatusText(row.taskStatus) === 'выполнено'">
+            <q-icon name="check_circle" class="text-green" />
+            <span class="text-green">{{ getStatusText(row.taskStatus) }}</span>
+          </template>
+        </div>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script setup>
@@ -59,11 +56,17 @@ import quasarIconSet from "quasar/icon-set/svg-material-icons";
 
 const { onResult: taskResult, refetch: taskRefetch } = useQuery(getTasks);
 
-const tasks = ref([
-  {
-    first: true,
-  },
-]);
+const tasks = ref([]);
+
+const getStatusText = (taskStatus) => {
+  if (taskStatus == "8407796538990824904") {
+    return "надо сделать";
+  } else if (taskStatus == "7045273205012284690") {
+    return "в процессе";
+  } else if (taskStatus == "2406017079472962662") {
+    return "выполнено";
+  }
+};
 
 // Добавляем свою иконку в набор иконок Quasar
 quasarIconSet.table.mySortIcon = "прив";
@@ -83,6 +86,25 @@ const addNewTask = () => {
 </script>
 
 <style scoped>
+.my-text-green {
+  color: #15803d;
+}
+.my-text-orange {
+  color: #c2410c;
+}
+.my-text-blue {
+  color: #1d4ed8;
+}
+.my-color {
+  background-color: #d7c9e2 !important;
+}
+.border-bottom {
+  border-bottom: 1px solid #9c9c9c !important;
+}
+.bg-color {
+  background-color: #eee8f3;
+}
+
 .q-table tbody td {
   font-size: 16px;
 }
