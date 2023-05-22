deccac2d92e484b6a98b22cfde89d6d8a70a778f
Paronim <Zreba.v@gmial.com>
2023-05-22 18:31:21 +0300

    Изменение верстки

--- src/components/ClubOrderInformation.vue
+++ src/components/ClubOrderInformation.vue
@@ -1,34 +1,38 @@
 <template>
   <section>
-    <section class="q-mx-auto q-mt-md wrapper-page-order-info">
-      <section class="flex items-baseline">
-        <h4 class="text-h4 q-mb-md q-mr-sm">
+    <section class="wrapper-page-order-info">
+      <section class="flex items-baseline c-mb-24">
+        <h4 class="c-f-f-f c-f-w-400 c-f-s-32 c-f-h-40 c-mr-32">
           просмотр заказа {{ props.order.id }}
         </h4>
 
-        <c-order-status :status="order_test" />
+        <c-order-status :status="props.order.status" />
       </section>
 
-      <section>
-        <h5 class="text-section-header q-mb-md">{{ props.order.name }}</h5>
+      <section class="c-mb-24">
+        <p class="c-f-f-p c-f-w-400 c-f-s-20 c-f-h-28 c-mb-24">
+          {{ props.order.name }}
+        </p>
       </section>
 
-      <section class="q-mb-xl flex justify-start items-baseline">
-        <h5 class="q-mb-xs q-mr-sm text-section-header">Заказчик</h5>
-        <p class="text-content-component-costumer">
+      <section class="q-mb-xl flex justify-start items-baseline c-mb-68">
+        <h5 class="q-mb-xs q-mr-sm c-f-f-p c-f-w-400 c-f-s-20 c-f-h-28">
+          Заказчик
+        </h5>
+        <p class="c-f-f-m c-f-w-400 c-f-s-16 c-f-h-24" style="color: #581c87">
           {{ props.order.customer }}
         </p>
       </section>
 
-      <section>
-        <h4 class="block-control-response-header">Отклик</h4>
+      <section class="c-mb-64">
+        <h4 class="c-f-f-p c-f-w-400 c-f-s-32 c-f-h-40 c-mb-24">Отклик</h4>
 
         <q-item-section
           class="block-control-response flex column justify-around"
         >
-          <section class="q-mt-lg q-mb-lg flex justify-start no-wrap">
+          <section class="flex justify-start no-wrap">
             <div
-              class="image-block-control-response q-ml-xl q-mr-md flex items-center justify-center"
+              class="image-block-control-response c-ml-32 c-mt-32 c-mr-24 flex items-center justify-center"
             >
               <q-icon
                 size="45px"
@@ -39,18 +43,20 @@
 
             <div class="flex column wrap content-block-control-response">
               <h4
-                class="q-mb-md content-block-control-response-header-text text-weight-medium"
+                class="c-mt-40 c-mb-12 c-f-f-p c-f-w-700 c-f-s-28 c-f-h-32"
+                style="color: #4b1873"
               >
                 Lorem ipsum dolor sit.
               </h4>
               <h6
-                class="q-mb-md content-block-control-response-first text-weight-regular"
+                class="c-mb-16 c-f-f-m c-f-w-400 c-f-s-16 c-f-h-24 c-c-grey-2"
               >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Numquam, incidunt similique quisquam esse quaerat quibusdam.
               </h6>
               <h6
-                class="content-block-control-response-last text-weight-regular"
+                class="c-f-f-m c-f-w-400 c-f-s-12 c-f-h-16"
+                style="color: #581c87"
               >
                 Ознакомьтесь с командой-кандидатом, утвердите или откажитесь от
                 неё, чтобы ИИ нашёл вам другого исполнителя.
@@ -58,25 +64,27 @@
             </div>
           </section>
 
-          <section class="flex items-center q-mb-md">
-            <div class="q-ml-xl date-response">11.11.2011</div>
+          <section class="flex items-end c-mb-32">
+            <div class="c-ml-32 c-f-f-m c-f-w-400 c-f-s-16 c-f-h-24 c-c-grey-2">
+              11.11.2011
+            </div>
 
             <c-button
-              class="q-ml-xl"
+              class="c-ml-32"
               :label="'Перейти к профилю'"
               :textColor="'white'"
               :background="true"
             />
 
             <c-button
-              class="q-ml-md"
+              class="c-ml-32"
               :label="'Утвердить исполнителя'"
               :textColor="'white'"
               :background="true"
             />
 
             <c-button
-              class="q-ml-md"
+              class="c-ml-32"
               :label="'Отказаться'"
               :textColor="'white'"
               :background="true"
@@ -85,23 +93,29 @@
         </q-item-section>
       </section>
 
-      <section class="q-mt-xl">
-        <h5 class="q-mt-sm text-section-header">Что требуется сделать</h5>
+      <section class="c-mb-64">
+        <h5 class="c-mb-24 c-f-f-p c-f-w-400 c-f-s-20 c-f-h-28 c-c-grey-1">
+          Что требуется сделать
+        </h5>
         <div class="flex justify-start">
-          <div class="flex wrap q-mt-lg">
+          <div class="flex wrap">
             <div
               v-for="todo in props.order.todos"
-              class="flex column justify-start q-mr-md"
+              class="flex column justify-start c-mr-24"
             >
-              <p class="text-content-component-todo q-mb-lg">
+              <p
+                class="text-content-component-todo c-f-f-m c-f-w-500 c-f-s-14 c-f-h-20 c-mb-24"
+              >
                 {{ todo }}
               </p>
               <div
-                class="text-content-component"
+                class="text-content-component c-mb-12"
                 v-for="fun in props.order.functions"
               >
                 <ul>
-                  <li class="q-ml-md q-mb-md">
+                  <li
+                    class="q-ml-md c-f-f-m c-f-w-500 c-f-s-14 c-f-h-20 c-c-grey-2 c-mb-12"
+                  >
                     {{ fun }}
                   </li>
                 </ul>
@@ -111,37 +125,53 @@
         </div>
       </section>
 
-      <section class="q-mt-xl">
-        <div class="q-mt-sm q-mb-sm flex justify-start items-center">
-          <h5 class="text-section-header" style="margin: 0">Описание заказа</h5>
+      <section class="c-mb-64">
+        <div class="flex justify-start items-baseline c-mb-16">
+          <h5 class="c-f-f-p c-f-w-400 c-f-s-20 c-f-h-28 c-c-grey-1 c-mr-32">
+            Описание заказа
+          </h5>
 
           <div
             v-if="order.consultation"
             class="flex justify-start items-center"
           >
-            <p class="text-content-component-cons">Мне нужна консультация</p>
+            <p
+              class="c-f-f-m c-f-w-400 c-f-s-12 c-f-h-16"
+              style="margin: 0; color: #581c87"
+            >
+              Мне нужна консультация
+            </p>
           </div>
         </div>
 
-        <p class="text-content-component style-information-block">
+        <p
+          class="c-f-f-m c-f-w-400 c-f-s-16 c-f-h-24 c-c-grey-2 style-information-block"
+        >
           {{ order.description }}
         </p>
       </section>
 
-      <section class="q-mt-xl">
-        <h5 class="q-mt-sm q-mb-lg text-section-header">Файлы и документы</h5>
-
-        {{ props.order.files }}
+      <section class="c-mb-64">
+        <h5 class="c-mb-32 c-f-f-p c-f-w-400 c-f-s-20 c-f-h-28 c-c-grey-1">
+          Файлы и документы
+        </h5>
+        <div
+          v-if="props.order.files === null"
+          class="c-f-f-m c-f-w-400 c-f-s-16 c-f-h-24 c-c-grey-2"
+        >
+          Файлов нет
+        </div>
+        <div v-else>{{ props.order.files }}</div>
       </section>
 
       <section class="flex justify-start items-baseline">
-        <div class="q-mt-xl" style="margin-right: 224px">
-          <h5 class="q-mt-sm q-mb-lg text-section-header">
+        <div class="c-mr-172">
+          <h5 class="c-mb-24 c-f-f-p c-f-w-400 c-f-s-20 c-f-h-28 c-c-grey-1">
             Желаемая стоимость
           </h5>
           <div class="flex">
             <p
-              class="text-content-component-cost-and-date q-mr-xl flex items-center"
+              class="c-f-f-m c-f-w-500 c-f-s-16 c-f-h-20 c-c-grey-2 c-mr-76 flex items-center"
             >
               от {{ props.order.price_start }}
               <q-icon
@@ -150,7 +180,9 @@
                 name="img:/assets/icons/orderIcons/ruble.svg"
               ></q-icon>
             </p>
-            <p class="text-content-component-cost-and-date flex items-center">
+            <p
+              class="c-f-f-m c-f-w-500 c-f-s-16 c-f-h-20 c-c-grey-2 flex items-center"
+            >
               до {{ props.order.price_end }}
               <q-icon
                 class="q-ml-sm"
@@ -161,12 +193,12 @@
           </div>
         </div>
 
-        <div class="q-mt-xl">
-          <h5 class="q-mt-sm q-mb-lg text-section-header">
+        <div>
+          <h5 class="c-mb-24 c-f-f-p c-f-w-400 c-f-s-20 c-f-h-28 c-c-grey-1">
             Желаемый срок готовности
           </h5>
 
-          <div class="text-content-component-cost-and-date">
+          <div class="c-f-f-m c-f-w-500 c-f-s-16 c-f-h-20 c-c-grey-2">
             {{ props.order.date_complete }}
           </div>
         </div>
@@ -187,12 +219,12 @@ const props = defineProps({
   },
 });
 
-const order_test = ref({
-  id: "7718095163914291112",
-  label: "Поиск исполнителя",
-  order: 0,
-  color: "#FF9646",
-});
+// const order_test = ref({
+//   id: "7718095163914291112",
+//   label: "Поиск исполнителя",
+//   order: 0,
+//   color: "#FF9646",
+// });
 
 console.log(props.order);
 
@@ -200,72 +232,14 @@ const colorStatus = ref("#9236DA");
 </script>
 
 <style scoped lang="scss">
-.date-response {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 24px;
-  display: flex;
-  align-items: flex-end;
-  color: #3f3f46;
-}
-.content-block-control-response-last {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 16px;
-  color: #581c87;
-}
-.content-block-control-response-first {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 24px;
-  color: #3f3f46;
-}
-.block-control-response-header {
-  font-family: "Play";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 32px;
-  line-height: 40px;
-  color: #27272a;
-}
-.text-content-component-cost-and-date {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 20px;
-  color: #3f3f46;
-}
 .style-information-block {
   border: 2px solid #d4d4d8;
   border-radius: 10px;
   padding: 6px 7px 6px 7px;
 }
-.text-section-header {
-  font-family: "Play";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 28px;
-  color: #27272a;
-}
-.content-block-control-response-header-text {
-  font-family: "Play";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 28px;
-  line-height: 32px;
-  color: #4b1873;
-}
 .wrapper-page-order-info {
-  max-width: 846px;
-  padding-bottom: 171px;
+  max-width: 1024px;
+  padding: 64px 0 200px 32px;
 }
 .color-type-status {
   width: 12px;
@@ -273,6 +247,7 @@ const colorStatus = ref("#9236DA");
   border-radius: 25px;
 }
 .block-control-response {
+  max-width: 820px;
   background: #ffffff;
   box-shadow: 0px 38px 15px rgba(0, 0, 0, 0.01),
     0px 21px 13px rgba(0, 0, 0, 0.03), 0px 10px 10px rgba(0, 0, 0, 0.04),
@@ -289,42 +264,145 @@ const colorStatus = ref("#9236DA");
 .content-block-control-response {
   max-width: 612px;
 }
-.text-content-component-costumer {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 24px;
-  color: #581c87;
-}
-.text-content-component {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 20px;
-  color: #3f3f46;
-}
-.text-content-component-todo {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 20px;
 
-  border-radius: 25px;
+.text-content-component-todo {
+  border-radius: 100px;
   color: white;
   background: linear-gradient(101.75deg, #4c1d95 4.25%, #881d95 96.95%);
   padding: 10px 16px 10px 16px;
 }
-.text-content-component-cons {
-  font-family: "Montserrat";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 20px;
-  padding-left: 32px;
-  margin: 0;
-  color: #77479d;
+
+.c-f-f {
+  &-m {
+    font-family: "Montserrat";
+    font-style: normal;
+  }
+
+  &-p {
+    font-family: "Play";
+    font-style: normal;
+  }
+
+  &-f {
+    font-family: "Furore";
+    font-style: normal;
+  }
+}
+
+.c-f-w {
+  &-700 {
+    font-weight: 700;
+  }
+  &-500 {
+    font-weight: 500;
+  }
+  &-400 {
+    font-weight: 400;
+  }
+}
+.c-f-s {
+  &-12 {
+    font-size: 12px;
+  }
+  &-14 {
+    font-size: 14px;
+  }
+  &-16 {
+    font-size: 16px;
+  }
+  &-20 {
+    font-size: 20px;
+  }
+  &-28 {
+    font-size: 28px;
+  }
+  &-32 {
+    font-size: 32px;
+  }
+}
+.c-f-h {
+  &-16 {
+    line-height: 16px;
+  }
+  &-20 {
+    line-height: 20px;
+  }
+  &-24 {
+    line-height: 24px;
+  }
+  &-28 {
+    line-height: 28px;
+  }
+  &-32 {
+    line-height: 32px;
+  }
+  &-40 {
+    line-height: 40px;
+  }
+}
+
+.c-c {
+  &-grey-1 {
+    color: #27272a;
+  }
+  &-grey-2 {
+    color: #3f3f46;
+  }
+}
+
+.c {
+  &-mt {
+    &-32 {
+      margin-top: 32px;
+    }
+    &-40 {
+      margin-top: 40px;
+    }
+    &-64 {
+      margin-top: 64px;
+    }
+  }
+  &-mb {
+    &-24 {
+      margin-bottom: 24px;
+    }
+    &-26 {
+      margin-bottom: 26px;
+    }
+    &-32 {
+      margin-bottom: 32px;
+    }
+    &-12 {
+      margin-bottom: 12px;
+    }
+    &-16 {
+      margin-bottom: 16px;
+    }
+    &-68 {
+      margin-bottom: 68px;
+    }
+    &-64 {
+      margin-bottom: 64px;
+    }
+  }
+  &-ml {
+    &-32 {
+      margin-left: 32px;
+    }
+  }
+  &-mr {
+    &-32 {
+      margin-right: 32px;
+    }
+    &-24 {
+      margin-right: 24px;
+    }
+    &-76 {
+      margin-right: 76px;
+    }
+    &-172 {
+      margin-right: 172px;
+    }
+  }
 }
 </style>
