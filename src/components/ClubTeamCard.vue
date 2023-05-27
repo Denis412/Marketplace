6af865c89ec32b5f16cb8a4e4fefb6182ab82e95
Denis <47417617+Denis412@users.noreply.github.com>
2023-05-28 02:53:44 +0300

    Скрытие карточек заявок от команд при статусе Отклонена или Одобрена

--- src/components/ClubTeamCard.vue
+++ src/components/ClubTeamCard.vue
@@ -1,5 +1,6 @@
 <template>
   <q-card
+    v-if="isShowCard"
     class="team card-shadow rounded-borders-10 c-pa-32 cursor-pointer column no-wrap justify-between"
     @click="to()"
   >
@@ -30,13 +31,14 @@
         :application="application"
         :incoming="incoming"
         @accept="$emit('accept', application)"
+        @status-calc="hideCard"
       />
     </q-card-section>
   </q-card>
 </template>
 
 <script setup>
-import { inject } from "vue";
+import { inject, ref } from "vue";
 import { useRouter } from "vue-router";
 
 import CApplicationControls from "./ClubApplicationControls.vue";
@@ -48,6 +50,8 @@ const currentUser = inject("currentUser");
 const router = useRouter();
 
 const { deletingTeam, deleteTeam } = useTeamDelete();
+
+const isShowCard = ref(true);
 
 const { team, application, incoming } = defineProps({
   team: Object,
@@ -64,6 +68,12 @@ const to = async () => {
 };
 
 const teamDelete = async () => await deleteTeam(team, currentUser.value.subject_id);
+
+const hideCard = (status, application) => {
+  console.log("app", status, application);
+  if (incoming && status.label !== "Одобрена" && status.label !== "Отклонена")
+    isShowCard.value = false;
+};
 </script>
 
 <style lang="scss" scoped>
