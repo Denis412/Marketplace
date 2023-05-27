ac2ed9c973cceecd6dd65ee5ca4a564eaba40a5b
Nikita Androsenko <varzaid@gmail.com>
2023-05-27 18:17:18 +0300

    Страница ClubBusinessPage заполнена структурой для дальнейшей разработки

--- src/pages/ClubBusinessPage.vue
+++ src/pages/ClubBusinessPage.vue
@@ -4,7 +4,37 @@
 
     <main class="text-white relative-position" style="z-index: 1">
       <c-main-screen />
+      <c-directions />
       <c-string />
+      <c-clients />
+
+      <section>
+        <h4 class="text-h4">
+          Реализуйте свои бизнес-идеи с it-специалистами, которым можно доверять
+        </h4>
+        <q-btn>Вступить в 1Т Клуб</q-btn>
+      </section>
+
+      <section>
+        <h3 class="text-h3">Лучшие it-команды 1т клуба</h3>
+        <div>слайдер</div>
+      </section>
+
+      <section>
+        <h3 class="text-h3">Задачи, которые решили участинки в 1т клубе</h3>
+        <div>слайдер</div>
+      </section>
+
+      <section>
+        <h3 class="text-h3">Часто задаваемые вопросы</h3>
+        <p>Какие бизнес-задачи поможет решить 1Т клуб?</p>
+        <p>
+          Могу ли я принять участие в 1Т Клубе, если еще не предприниматель, а только начинаю свое
+          дело?
+        </p>
+        <p>Что будет, если команды 1Т Клуба не справятся с проектом?</p>
+        <p>Какие требования к резидентам 1Т Клуба?</p>
+      </section>
     </main>
   </q-page>
 </template>
@@ -12,5 +42,7 @@
 <script setup>
 import CBackgroundSpot from "components/Landing/ClubBackgroundSpot.vue";
 import CMainScreen from "components/Landing/ClubMainScreen.vue";
+import CDirections from "components/Landing/ClubDirections.vue";
 import CString from "components/Landing/ClubString.vue";
+import CClients from "components/Landing/ClubClients.vue";
 </script>
