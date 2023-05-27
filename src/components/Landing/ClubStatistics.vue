b0c8597d07cc99c593e6c03a3aabd9ecc03de9de
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-05-27 18:01:40 +0300

    Подготовил локигу для слайдера участников клуба

--- src/components/Landing/ClubStatistics.vue
+++ src/components/Landing/ClubStatistics.vue
@@ -18,16 +18,27 @@
     </div>
 
     <div class="partitians">
-      <h3 class="text-subtitle1">Почетные участники 1Т Клуба</h3>
-      <div class="partitians-faces relative-position">
+      <h3 class="text-subtitle1 q-mt-xl q-pt-xl">Участники 1Т Клуба</h3>
+      <div v-if="!flag" class="partitians-faces relative-position">
         <c-partitian :index="index" v-for="index in 33" :key="index" />
+      </div>
+      <div v-if="flag" class="partitians-faces relative-position">
+        <c-partitian :index="index+33" v-for="index in 33" :key="index" />
+      </div>
+      <div class="q-mt-lg">
+      <q-btn @click="flag = !flag" unelevated dense size="xl" :disable="!flag" class="q-mr-md" :icon="'chevron_left'"/>
+      <q-btn @click="flag = !flag" unelevated dense size="xl" :disable="flag" class="q-mr-md" :icon="'chevron_right'"/>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import CPartitian from "./ClubPartitian.vue";
+
+const flag = ref(false)
+const chet = ref(33)
 </script>
 
 <style lang="scss" scoped>
@@ -43,4 +54,5 @@ import CPartitian from "./ClubPartitian.vue";
 .text-violet-3{
   color: rgba(144, 67, 180, 1);
 }
+
 </style>
