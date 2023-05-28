12e67ace4ed8401555492caaa35e748793c4807d
Nikita Androsenko <varzaid@gmail.com>
2023-05-28 14:12:11 +0300

    Доработан блок с вопросами, добавлены компоненты List и Item для него

--- src/components/Landing/ClubFAQ.vue
+++ src/components/Landing/ClubFAQ.vue
@@ -1,96 +1,53 @@
 <template>
   <section class="text-center container">
     <h3 class="text-h3 text-left title">Часто задаваемые вопросы</h3>
-    <q-expansion-item
-      class="text-subtitle5 text-left"
-      label="Какие бизнес-задачи поможет решить 1Т клуб?"
-      :header-style="{ paddingLeft: 0 }"
-      expand-icon-class="text-white"
-    >
-      <q-card class="text-body2">
-        <q-card-section class="q-pl-none">
-          <ul>
-            <li>Подбор IT-специалистов и укомплектованных IT-команд</li>
-            <li>Обмен знаниями и опытом с резидентами 1Т Клуба</li>
-            <li>Углубление социальных связей через общение на онлайн и офлайн мероприятиях</li>
-            <li>Поддержка и консультация от экспертов в IT сфере</li>
-          </ul>
-        </q-card-section>
-      </q-card>
-    </q-expansion-item>
-    <q-expansion-item
-      class="text-subtitle5 text-left"
-      label="Могу ли я принять участие в 1Т Клубе, если еще не предприниматель, а только начинаю свое дело?"
-      :header-style="{ paddingLeft: 0 }"
-      expand-icon-class="text-white"
-    >
-      <q-card class="text-body2">
-        <q-card-section class="q-pl-none">
-          Да, вы можете принять участие в 1T Клубе, даже если вы только начинаете свой бизнес. 1T
-          Клуб — это сообщество предпринимателей, новаторов и владельцев бизнеса, которые стремятся
-          к успеху в своих областях.
-        </q-card-section>
-      </q-card>
-    </q-expansion-item>
-    <q-expansion-item
-      class="text-subtitle5 text-left"
-      label="Что будет, если команды 1Т Клуба не справятся с проектом?"
-      :header-style="{ paddingLeft: 0 }"
-      expand-icon-class="text-white"
-    >
-      <q-card class="text-body2">
-        <q-card-section class="q-pl-none">
-          В случае возникновения недопонимания или неудовлетворенности качеством IT-продукта
-          заказчиком подключается платформа 1Т Клуба для урегулирования споров. Решением может стать
-          возврат денег за услугу (частичный или полный) либо гарантия выполнения услуги экспертами
-          1Т клуба.
-        </q-card-section>
-      </q-card>
-    </q-expansion-item>
-    <q-expansion-item
-      class="text-subtitle5 text-left"
-      label="Какие требования к резидентам 1Т Клуба?"
-      :header-style="{ paddingLeft: 0 }"
-      expand-icon-class="text-white"
-    >
-      <q-card class="text-body2">
-        <q-card-section class="q-pl-none">
-          Вы можете вступить в 1Т Клуб, если вы владелец бизнеса или фаундер (основатель)
-          IT-стартапа, технологической компании или социально значимого проекта
-        </q-card-section>
-      </q-card>
-    </q-expansion-item>
+    <c-faq-list :questions="questionList" />
   </section>
 </template>
 <script setup>
-const cls = {
-  class: "cls",
-};
+import CFaqList from "./ClubFAQList.vue";
+import { ref } from "vue";
+
+const questionList = ref([
+  {
+    id: 1,
+    label: "Какие бизнес-задачи поможет решить 1Т клуб?",
+    content: [
+      "Подбор IT-специалистов и укомплектованных IT-команд",
+      "Обмен знаниями и опытом с резидентами 1Т Клуба",
+      "Углубление социальных связей через общение на онлайн и офлайн мероприятиях",
+      "Поддержка и консультация от экспертов в IT сфере",
+    ],
+  },
+  {
+    id: 2,
+    label:
+      "Могу ли я принять участие в 1Т Клубе, если еще не предприниматель, а только начинаю свое дело?",
+    content: [
+      "Да, вы можете принять участие в 1T Клубе, даже если вы только начинаете свой бизнес. 1T Клуб — это сообщество предпринимателей, новаторов и владельцев бизнеса, которые стремятся к успеху в своих областях.",
+    ],
+  },
+  {
+    id: 3,
+    label: "Что будет, если команды 1Т Клуба не справятся с проектом?",
+    content: [
+      "В случае возникновения недопонимания или неудовлетворенности качеством IT-продукта заказчиком подключается платформа 1Т Клуба для урегулирования споров. Решением может стать возврат денег за услугу (частичный или полный) либо гарантия выполнения услуги экспертами 1Т клуба.",
+    ],
+  },
+  {
+    id: 4,
+    label: "Какие требования к резидентам 1Т Клуба?",
+    content: [
+      "Вы можете вступить в 1Т Клуб, если вы владелец бизнеса или фаундер (основатель) IT-стартапа, технологической компании или социально значимого проекта.",
+    ],
+  },
+]);
 </script>
 <style scoped lang="scss">
-li {
-  list-style-position: inside;
-  list-style-type: none;
-  &::before {
-    content: "• ";
-  }
-}
-
 .title {
   margin-bottom: 50px;
 }
 .container {
   margin-bottom: 160px;
-}
-.q-expansion-item {
-  border-bottom: 2px solid #9043b4;
-}
-.q-card {
-  background-color: transparent;
-}
-
-.my-custom-class {
-  background-color: black;
-  padding: 0;
 }
 </style>
---
+++ src/components/Landing/ClubFAQItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-expansion-item
+    class="text-subtitle5 text-left"
+    :label="label"
+    :header-style="{ paddingLeft: 0 }"
+    expand-icon-class="text-white"
+  >
+    <q-card class="text-body2">
+      <q-card-section v-if="content.length > 1" class="q-pl-none">
+        <ul>
+          <li v-for="contentItem in content" :key="contentItem">{{ contentItem }}</li>
+        </ul>
+      </q-card-section>
+      <q-card-section v-else class="q-pl-none">
+        {{ content[0] }}
+      </q-card-section>
+    </q-card>
+  </q-expansion-item>
+</template>
+<script setup>
+const { label, content } = defineProps({
+  label: String,
+  content: Array,
+});
+</script>
+<style scoped lang="scss">
+.q-expansion-item {
+  border-bottom: 2px solid #9043b4;
+}
+.q-card {
+  background-color: transparent;
+}
+li {
+  list-style-position: inside;
+  list-style-type: none;
+  &::before {
+    content: "• ";
+  }
+}
+</style>
