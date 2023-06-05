f9a71d6d523fee62004f021a98172f43e5498038
Denis <47417617+Denis412@users.noreply.github.com>
2023-06-05 14:33:03 +0300

    Изменил нерабочий запрос на странице редактирования команды

--- src/pages/ClubTeamSettingsPage.vue
+++ src/pages/ClubTeamSettingsPage.vue
@@ -1,30 +1,20 @@
 <template>
   <q-page class="c-pt-32 c-pl-32 c-pr-32">
-    <c-team-settings v-if="team" />
+    <c-team-settings v-if="currentTeam" />
   </q-page>
 </template>
 
 <script setup>
 import CTeamSettings from "src/components/ClubTeamSettings.vue";
-import teamApi from "src/sdk/team";
-import { computed, provide } from "vue";
+import TeamService from "src/sevices/TeamService";
+import { provide } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 
-const { result: currentTeam } = teamApi.paginateTeams({
-  page: 1,
-  perPage: 1,
-  where: {
-    column: "id",
-    operator: "EQ",
-    value: route.params.id,
-  },
-});
+const { result: currentTeam } = TeamService.fetchTeamById(route.params.id);
 
-const team = computed(() => currentTeam.value?.paginate_team?.data[0]);
-
-provide("currentTeam", team);
+provide("currentTeam", currentTeam);
 </script>
 
 <style scoped lang="scss"></style>
